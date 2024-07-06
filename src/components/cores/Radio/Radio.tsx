c79a356b68ff2055c282f343806487223ab628ce
yuanningd <65219674+yuanningd@users.noreply.github.com>
2024-07-06 21:42:04 +1000

    feat: forward ref to radio component

--- src/components/cores/Radio/Radio.tsx
+++ src/components/cores/Radio/Radio.tsx
@@ -1,4 +1,9 @@
-import React, { ChangeEvent, InputHTMLAttributes, useContext } from 'react'
+import React, {
+  ChangeEvent,
+  forwardRef,
+  InputHTMLAttributes,
+  useContext,
+} from 'react'
 import { RadioGroupContext } from './group/RadioGroupContext'
 import { RadioWrapper, RadioInput } from './Radio.styles'
 
@@ -11,56 +16,64 @@ interface RadioProps
   checked?: boolean
 }
 
-const Radio = ({
-  value,
-  defaultChecked,
-  onChange,
-  disabled,
-  children,
-  name,
-  checked,
-  ...rest
-}: RadioProps) => {
-  const radioGroup = useContext(RadioGroupContext)
-  const radioGroupExists = radioGroup.selectedValue !== null
-  const isChecked = radioGroupExists
-    ? radioGroup.selectedValue === value
-    : checked
+const Radio = forwardRef<HTMLInputElement, RadioProps>(
+  (
+    {
+      value,
+      defaultChecked,
+      onChange,
+      disabled,
+      children,
+      name,
+      checked,
+      ...rest
+    },
+    ref,
+  ) => {
+    const radioGroup = useContext(RadioGroupContext)
+    const radioGroupExists = radioGroup.selectedValue !== null
+    const isChecked = radioGroupExists
+      ? radioGroup.selectedValue === value
+      : checked
 
-  const isControlled = isChecked !== undefined
+    const isControlled = isChecked !== undefined
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (radioGroupExists) {
-      radioGroup.onChange(event)
-    } else if (onChange) {
-      onChange(event)
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      if (radioGroupExists) {
+        radioGroup.onChange(event)
+      } else if (onChange) {
+        onChange(event)
+      }
     }
-  }
 
-  return (
-    <label>
-      <RadioWrapper
-        style={{
-          backgroundColor:
-            radioGroupExists && isChecked
-              ? radioGroup.checkedBackgroundColor
-              : 'transparent',
-        }}
-        {...rest}
-      >
-        <RadioInput
-          type="radio"
-          value={value}
-          name={name}
-          checked={isControlled ? isChecked : undefined}
-          defaultChecked={isControlled ? undefined : defaultChecked}
-          onChange={handleChange}
-          disabled={disabled}
-        />
-        {children}
-      </RadioWrapper>
-    </label>
-  )
-}
+    return (
+      <label>
+        <RadioWrapper
+          style={{
+            backgroundColor:
+              radioGroupExists && isChecked
+                ? radioGroup.checkedBackgroundColor
+                : 'transparent',
+          }}
+          {...rest}
+        >
+          <RadioInput
+            type="radio"
+            value={value}
+            name={name}
+            checked={isControlled ? isChecked : undefined}
+            defaultChecked={isControlled ? undefined : defaultChecked}
+            onChange={handleChange}
+            disabled={disabled}
+            ref={ref}
+          />
+          {children}
+        </RadioWrapper>
+      </label>
+    )
+  },
+)
+
+Radio.displayName = 'Radio'
 
 export default Radio
